d322c411f54ae64dd5713d76b4fcc470c555a856
Ryan White <ryanwhite04@gmail.com>
2020-10-18 18:06:19 +0800

    Use inbuilt tinyusb library

--- webusb/webusb.ino
+++ webusb/webusb.ino
@@ -8,62 +8,50 @@
  All text above, and the splash screen below must be included in
  any redistribution
 *********************************************************************/
-#include "Adafruit_TinyUSB.h"
+#include <Adafruit_TinyUSB.h>
 Adafruit_USBD_WebUSB usb_web;
 WEBUSB_URL_DEF(landingPage, 1, "speedence.com");
 
-int led_pin = LED_BUILTIN;
-int previous = 0;
-int wheel = 0;
-int pedal = 1;
-int wait;
+int wheel = 1;
+int wheelPrevious = 0;
+int wheelWait;
+
+int pedal = 2;
+int pedalPrevious = 0;
+int pedalWait;
+
 void setup() {
-  pinMode(led_pin, OUTPUT);
+ 
   pinMode(11, INPUT_PULLUP);
   usb_web.setLandingPage(&landingPage);
   usb_web.setLineStateCallback(line_state_callback);
   usb_web.begin();
   Serial.begin(115200);
   while(!USBDevice.mounted()) delay(1);
-  Serial.println("TinyUSB WebUSB Serial example");
   randomSeed(analogRead(A0));
-  wait = random(200, 500);
- 
+  pedalWait = random(200, 500);
+  pedalWait = random(500, 800);
   attachInterrupt(digitalPinToInterrupt(12), sendWheel, LOW);
   attachInterrupt(digitalPinToInterrupt(13), sendPedal, LOW);
 }
 
-// function to echo to both Serial and WebUSB
-void echo_all(char chr) {
-  Serial.write(chr);
-  // print extra newline for Serial
-  if ( chr == '\r' ) Serial.write('\n');
-  usb_web.write(chr);
-}
-
 void loop() {
-  // delay(100);
-  // Serial.println(30000);
-  // Serial.println(analogRead(A0));
   int now = millis();
-  if (now - previous > wait && !digitalRead(11)) {
-    previous = now;
-    wait = random(200, 500);
-    usb_web.println(counter++);
+  if (now - wheelPrevious > wheelWait && !digitalRead(11)) {
+    wheelPrevious = now;
+    wheelWait = random(200, 500);
+    wheel += 2;
+    relay(wheel);
+  }
+  if (now - pedalPrevious > pedalWait && !digitalRead(11)) {
+    pedalPrevious = now;
+    pedalWait = random(500, 800);
+    pedal += 2;
+    relay(pedal);
   }
 }
 
-void sendWheel() {
-  wheel += 2;
-  usb_web.println(wheel);
-}
-
-void sendPedal() {
-  pedal += 2;
-  usb_web.println(pedal);
-}
-
-void line_state_callback(bool connected) {
-  digitalWrite(led_pin, connected);
-  if (connected) usb_web.print("Connected");
-}
+void sendWheel() { wheel += 2; relay(wheel); }
+void sendPedal() { pedal += 2; relay(pedal); }
+void relay(int message) { usb_web.print(message); Serial.print(message); }
+void line_state_callback(bool connected) { if (connected) relay(0); }
