7a9695457080ace63186e14ff81a4e4163fc451d
Ryan White <ryanwhite04@gmail.com>
2020-10-20 19:40:12 +0800

    Trigger interrupt on Falling rather than Low

--- webusb/basic.ino
+++ webusb/basic.ino
@@ -5,15 +5,20 @@ WEBUSB_URL_DEF(landingPage, 1, "speedence.com");
 int wheel = 1;
 int pedal = 2;
 void setup() {
+  pinMode(12, INPUT_PULLUP);
+  pinMode(13, INPUT_PULLUP);
   webusb.setLandingPage(&landingPage); // When plugged in to compatible devices, will ask user to go to this page
   webusb.setLineStateCallback(line_state_callback); // I don't know what this does to be honest
   webusb.begin(); // Start up usb_web
+  Serial.begin(115200);
   while(!USBDevice.mounted()) delay(1);
-  attachInterrupt(digitalPinToInterrupt(12), sendWheel, LOW);
-  attachInterrupt(digitalPinToInterrupt(13), sendPedal, LOW);
+  attachInterrupt(digitalPinToInterrupt(12), sendWheel, FALLING);
+  attachInterrupt(digitalPinToInterrupt(13), sendPedal, FALLING);
 }
 
-void loop() {}
-void sendWheel() { wheel += 2; webusb.print(wheel); }
-void sendPedal() { pedal += 2; webusb.print(pedal); }
-void line_state_callback(bool connected) { if (connected) webusb.print(0); }
+void loop() {
+  delay(1);
+}
+void sendWheel() { wheel += 2; Serial.print(wheel); webusb.print(wheel); }
+void sendPedal() { pedal += 2; Serial.print(wheel); webusb.print(pedal); }
+void line_state_callback(bool connected) { if (connected) { Serial.print(0); webusb.print(0); } }
